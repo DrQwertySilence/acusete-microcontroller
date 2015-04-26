ad50568b63883d6a161ab622c1109bb28a4417e3
DrQwertySilence <maximiliano_arenas1@hotmail.com>
2015-04-26 15:33:09 -0300

    Added project files

---
+++ pins.h
@@ -0,0 +1,8 @@
+#ifndef PINS_H
+#define PINS_H
+
+int DS18S20_DPIN = 2; // Temperature sensor digital pin
+int MQ_4_APIN = 0; // Natural gas sensor analog pin
+int LED_DPIN = 12; // LED
+
+#endif // PINS_H
